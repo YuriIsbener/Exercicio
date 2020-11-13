4ee88cd4e4168d25f8588d48176445dff6aedd78
Yuri Isbener <yurisuhett@gmail.com>
2020-11-13 16:47:13 -0300

    projeto concluido

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            int idadeMes;
+            int idadeDias;
+            int idadeMinutos;
+
+            Console.WriteLine("Qual sua idade?");
+            idade = int.Parse(Console.ReadLine());
+            
+            idadeMes = idade*12;
+            Console.WriteLine("Sua idade em meses é: "+idadeMes);
+
+            idadeDias = idade*365;
+            Console.WriteLine("Sua idade em dias é: "+idadeDias);
+
+            idadeMinutos = (idadeDias*24)*60;
+            Console.WriteLine("Sua idade em minutos é: "+idadeMinutos);
+
+
+
+        }
+    }
+}
